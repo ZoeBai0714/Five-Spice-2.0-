541f53d636612a57f6f0d7622467612847c01a29
Zoe Bai <42818214+ZoeBai0714@users.noreply.github.com>
2019-01-15 23:57:35 +0800

    Update five_spice.js
    Changed from app.listen(2020) to app.listen(process.env.PORT, '0.0.0.0') for deployment on clever cloud

--- five_spice.js
+++ five_spice.js
@@ -4,5 +4,5 @@ var controller=require('./controller/controller');
 app.set('view engine', 'ejs');  //set view engine
 app.use('/assets', express.static('assets'))  //insert stylesheet
 controller(app);
-app.listen(2020)
-console.log('You are listening to 2020 ')
+app.listen(process.env.PORT, '0.0.0.0') // original is app.listen(2020)
+console.log('You are listening to 8080 ')
